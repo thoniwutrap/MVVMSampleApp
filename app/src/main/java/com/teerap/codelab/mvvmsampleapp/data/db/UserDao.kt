598ec15c4acbf4b2680b48e8e3b71de93a5f0e2f
Thoniwut Rapeepisarn <thoniwut.rap@gmail.com>
2019-06-19 14:55:31 +0700

    add room database

---
+++ app/src/main/java/com/teerap/codelab/mvvmsampleapp/data/db/UserDao.kt
@@ -0,0 +1,20 @@
+package com.teerap.codelab.mvvmsampleapp.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.teerap.codelab.mvvmsampleapp.data.db.entities.CURRENT_USER_ID
+import com.teerap.codelab.mvvmsampleapp.data.db.entities.User
+
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(user : User) : Long
+
+    @Query("SELECT * FROM user WHERE uid = $CURRENT_USER_ID")
+    fun getUser() : LiveData<User>
+}
